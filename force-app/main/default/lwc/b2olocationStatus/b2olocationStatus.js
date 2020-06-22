541e41cde25c9e1a5093c3c62fd382e2e5ebb211
sebastianocostanzo <sebastianocostanzo@hotmail.com>
2020-06-22 10:05:54 -0700

    beta fix

--- force-app/main/default/lwc/b2olocationStatus/b2olocationStatus.js
+++ force-app/main/default/lwc/b2olocationStatus/b2olocationStatus.js
@@ -3,12 +3,12 @@ import { getListUi } from 'lightning/uiListApi';
 
 // This is a beta functionality that is not working currently in Summer '20
 // Use at your own risk
-import OFFICE_LOCATION_OBJECT from '@salesforce/schema/appyback__Location__c';
+//import OFFICE_LOCATION_OBJECT from '@salesforce/schema/Location__c';
 
 export default class B2olocationStatus extends LightningElement {
     @track sobjectResult;
     @wire(getListUi, {
-        objectApiName: OFFICE_LOCATION_OBJECT,
+        objectApiName: 'appyback__Location__c',
         listViewApiName: 'All'
     })
     wiredlistView({
